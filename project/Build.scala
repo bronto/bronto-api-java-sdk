bfdab1998dbaf15cdd0d0aca74fc8340d2ba549b
Marc Baumbach <marc.baumbach@gmail.com>
2014-06-05 09:22:13 -0400

    Updated Maven POMs and making SBT utilize them

--- project/Build.scala
+++ project/Build.scala
@@ -1,38 +1,5 @@
-import sbt._
-import Keys._
+import com.github.shivawu.sbt.maven.MavenBuild
 
-object Build extends sbt.Build {
-  object Common {
-    lazy val settings = Project.defaultSettings ++ Seq(
-      javacOptions in (Compile, run) ++= Seq("-Xlint:deprecation", "-Xlint:unchecked"),
-      version := "1.0.1-SNAPSHOT",
-      organization := "com.bronto",
-      publishArtifact in (Compile, packageDoc) := false,
-      crossVersion := CrossVersion.Disabled
-    )
-  }
-
-  lazy val root = Project(
-    "bronto-api",
-    file("."),
-    settings = Common.settings
-  ) aggregate (sdk, client)
-
-  lazy val app = Project(
-    "bronto-api-app",
-    file("app"),
-    settings = Common.settings
-  ) dependsOn sdk
-
-  lazy val client = Project(
-    "bronto-api-client",
-    file("client"),
-    settings = Common.settings
-  )
-
-  lazy val sdk = Project(
-    "bronto-api-sdk",
-    file("sdk"),
-    settings = Common.settings
-  ) dependsOn client
+object Build extends MavenBuild {
+    project("*")()
 }
